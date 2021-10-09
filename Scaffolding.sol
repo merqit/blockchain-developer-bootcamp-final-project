93d8068a48adae96295134186361399eb24f8bec
merqit <merquitt@sandiego.edu>
2021-10-09 12:18:03 -0700

    .

---
+++ Scaffolding.sol
@@ -0,0 +1,7 @@
+/// Users will have to register themselves to the contract///
+    function registerStudent(address _student) {
+            // registes the vote of the voter
+
+    };
+        
+    
